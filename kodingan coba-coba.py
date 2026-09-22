c780f37ff1e68023e8b3b7ca32424b3adbf64ff8
hikmalfauzan261003-cell <hikmalfauzan261003@gmail.com>
2026-09-22 13:57:56 +0700

    Fix title typo in Stock Opname Worksheet Generator

--- kodingan coba-coba.py
+++ kodingan coba-coba.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Stock Opname Khusus SUki", page_icon="📊", layout="wide"
 )
 
-st.title("📊 Stock Opname Worksheet Generator")
+st.title("📊 Stock Opname Worksuki Generator")
 st.write(
     "Halo Para Suki dan Member, Anda malas bikin worksheet? Sama saya juga Makanya saya bikin ini ya."
 )
